37291c53956ea3c954666608ed60fe3792462066
cscherho <cscherhoff@web.de>
2022-04-15 13:26:52 +0200

    Add add-new-expense component

---
+++ src/app/add-new-expenses/add-new-expenses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-expenses',
+  templateUrl: './add-new-expenses.component.html',
+  styleUrls: ['./add-new-expenses.component.css']
+})
+export class AddNewExpensesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
