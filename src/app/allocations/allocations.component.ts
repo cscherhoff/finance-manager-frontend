08865c5b78f7d01f6ea56d58f57011449d9c7a2b
cscherho <cscherhoff@web.de>
2022-07-21 22:38:24 +0200

    Ability to change and add categories and show sum

--- src/app/allocations/allocations.component.ts
+++ src/app/allocations/allocations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Allocation } from './data/Allocation';
+import { Category } from './data/Category';
 import { FixCost } from './data/FixCost';
 import { AllocationService } from './service/allocation.service';
 
@@ -12,9 +13,14 @@ export class AllocationsComponent implements OnInit {
   
   isUpdating: boolean = false;
   addFixcost: boolean = false;
+  addCategory: boolean = false;
   newFixcost: FixCost = {fixCostName: '', amount: 0};
+  newCategory: Category = {categoryName: '', budget: 0};
   public newValue: number = 0;
   income: number = 0;
+
+  fixCostSum: number = 0;
+  categorySum: number = 0;
 
   allocation: Allocation = {investment: 0.0, categories: [], fixCosts: []};
   allocationUpdate: Allocation = {investment: 0.0, categories: [], fixCosts: []};
@@ -28,7 +34,11 @@ export class AllocationsComponent implements OnInit {
 
   getAllocation(): void {
    this.allocationService.getAllocation()
-    .subscribe(allocationResponse => this.allocation = allocationResponse);
+    .subscribe(allocationResponse => {
+      this.allocation = allocationResponse;
+      this.calculateFixCostSum();
+      this.calculateCategorySum();
+    });
   }
 
   getIncome(): void {
@@ -40,22 +50,63 @@ export class AllocationsComponent implements OnInit {
     this.allocationService.postIncome(this.income);
   }
 
-  updateAllocation(i: number) {
+  updateFixcost(i: number) {
     this.newValue = this.allocation.fixCosts[i].amount;
     this.allocation.fixCosts[i].updating = true;
   }
 
-  submitUpdateAllocation(i: number) {
+  submitUpdateFixcost(i: number) {
     this.allocation.fixCosts[i].amount = this.newValue;
     this.allocation.fixCosts[i].updating = false;
+    this.calculateFixCostSum();
+  }
+
+  updateCategory(i: number) {
+    this.newValue = this.allocation.categories[i].budget;
+    this.allocation.categories[i].updating = true;
+  }
+
+  submitUpdateCategory(i: number) {
+    this.allocation.categories[i].budget = this.newValue;
+    this.allocation.categories[i].updating = false;
+    this.calculateCategorySum();
   }
 
   addNewFixcost() {
     this.addFixcost = true;
   }
 
+  addNewCategory() {
+    this.addCategory = true;
+  }
+
   submitNewFixcost() {
     this.allocation.fixCosts.push({fixCostName: this.newFixcost.fixCostName, amount: this.newFixcost.amount});
     this.addFixcost = false;
+    this.newFixcost.fixCostName = '';
+    this.newFixcost.amount = 0;
+    this.calculateFixCostSum();
+  }
+
+  submitNewCategory() {
+    this.allocation.categories.push({categoryName: this.newCategory.categoryName, budget: this.newCategory.budget});
+    this.addCategory = false;
+    this.newCategory.categoryName = '';
+    this.newCategory.budget = 0;
+    this.calculateCategorySum();
+  }
+
+  calculateFixCostSum() {
+    this.fixCostSum = 0;
+    this.allocation.fixCosts.forEach(fixCost => {
+      this.fixCostSum+= fixCost.amount;
+    });
+  }
+
+  calculateCategorySum() {
+    this.categorySum = 0;
+    this.allocation.categories.forEach(category => {
+      this.categorySum += category.budget;
+    })
   }
 }
--- src/app/allocations/data/Category.ts
+++ src/app/allocations/data/Category.ts
@@ -1,4 +1,5 @@
 export interface Category {
     categoryName: string;
     budget: number;
+    updating?: boolean;
 }
