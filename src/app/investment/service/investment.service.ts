a1131b0db444fefd99bc391c4b68658252ab70b1
cscherho <cscherhoff@web.de>
2022-08-20 16:20:32 +0200

    Delete unused lines

--- src/app/investment/service/investment.service.ts
+++ src/app/investment/service/investment.service.ts
@@ -4,10 +4,8 @@ import { delay, Observable, of } from 'rxjs';
 import { DepotEntry } from '../data/DepotEntryData';
 import { Profit } from '../data/ProfitData';
 import { Transaction } from '../data/Transaction';
-import { DEPOT_ENTRIES_MOCK } from '../mockData/mock-depotEntries';
 import { Security } from '../data/Security';
 import { PROFIT_MOCK } from '../mockData/mock-profits';
-import { ISIN_MOCK, TRANSACTION_MOCK } from '../mockData/mock-transactions';
 import { InvestmentInformation } from '../data/InvestmentInformation';
 
 @Injectable({
@@ -21,19 +19,12 @@ export class InvestmentService {
 
   constructor(private httpClient: HttpClient) { }
 
-  // getIsins(): Observable<string[]> {
-  //   return of(ISIN_MOCK).pipe(delay(5000));
-  // }
-
   getTransactions(): Observable<Transaction[]> {
     return this.httpClient.get<Transaction[]>('http://localhost:30005/investments/user/1/transactions');
-    // return of(TRANSACTION_MOCK).pipe(delay(500));
   }
 
   getDepotEntries(): Observable<DepotEntry[]> {
     return this.httpClient.get<DepotEntry[]>('http://localhost:30005/investments/user/1/depotEntries');
-    //   .subscribe(response => console.log(response));
-    // return of(DEPOT_ENTRIES_MOCK).pipe(delay(500));
   }
 
   getProfits(): Observable<Profit[]> {
@@ -42,12 +33,6 @@ export class InvestmentService {
 
   getIsinSecurityName(): Observable<Security[]> {
     return this.httpClient.get<Security[]>('http://localhost:30005/investments/user/0/securities');
-    // return of(
-    //   new Map<string, string>([
-    //     ["US1234567890", "Fielmann"],
-    //     ["FR0123456789", "Apple"]
-    // ])
-    // ).pipe(delay(8000));
   }
 
   postTransaction(transaction: Transaction): void {
